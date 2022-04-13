e7fb57bd310ef099289459d0fc4a53c9da805daa
zackhamin <ishaqhamin@gmail.com>
2022-04-13 23:00:05 +0100

    Added router and links to routes

--- src/App.js
+++ src/App.js
@@ -1,38 +1,20 @@
-import "./categories.styless.scss";
-import Categories from "./components/categories/categories";
+import Home from "./routes/home";
+import { Routes, Route } from "react-router-dom";
+import Navigation from "./routes/navbar/navbar";
+
+const Shop = () => {
+  return <h2>Shop page</h2>;
+};
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
-
   return (
     <div className="categories-container">
-      <Categories categoriesList={categories} />
+      <Routes>
+        <Route path="/" element={<Navigation />}>
+          <Route index element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+        </Route>
+      </Routes>
     </div>
   );
 };
