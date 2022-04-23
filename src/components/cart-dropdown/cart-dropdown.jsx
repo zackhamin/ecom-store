40248c0496e8aceffcea122dedf0c179d6670642
zackhamin <ishaqhamin@gmail.com>
2022-04-23 12:03:15 +0100

    Add cart down drop and icon

---
+++ src/components/cart-dropdown/cart-dropdown.jsx
@@ -0,0 +1,3 @@
+import "./cart-dropdown.styles.scss";
+const CartDropDown = () => {};
+export default CartDropDown;
--- src/routes/navbar/navbar.jsx
+++ src/routes/navbar/navbar.jsx
@@ -3,6 +3,8 @@ import { Fragment, useContext } from "react";
 import { ReactComponent as CrownLogo } from "../../assets/crown.svg";
 import { UserContext } from "../../contexts/user.context";
 import { signUserOut } from "../../utils/firebase/firebase";
+import CartIcon from "../../components/cart-icon/cart-icon";
+
 import "./navigation.styles.scss";
 const Navigation = () => {
   const { currentUser, setCurrentUser } = useContext(UserContext);
@@ -26,6 +28,7 @@ const Navigation = () => {
               Sign In
             </Link>
           )}
+          <CartIcon />
         </div>
       </div>
       <Outlet />
