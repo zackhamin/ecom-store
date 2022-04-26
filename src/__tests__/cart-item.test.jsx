c92eadb407d791a3121a77e8a8c9af09aa878ddd
ishaqhamin@gmail.com <zackhamin>
2022-04-26 09:32:17 +0100

    Add base testing libraries

---
+++ src/__tests__/cart-item.test.jsx
@@ -0,0 +1,17 @@
+import CartItem from "../components/cart-item/cart-item";
+
+import Todo from "../app/todo";
+import React from "react";
+import { mount } from "enzyme";
+
+test("TodoComponent renders the text inside it", () => {
+  const cartItem = {
+    name: "test-item",
+    quantity: 1,
+    price: 4.99,
+    imageUrl: "www.test-url.com",
+  };
+  const wrapper = mount(<CartItem cartItem={cartItem} />);
+  const p = wrapper.find(".cart-item-container");
+  expect(p.text()).toBe("test-item", 1, 4.99, "www.test-url.com");
+});
